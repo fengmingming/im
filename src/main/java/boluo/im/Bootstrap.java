c1958cb6979ef3a719ee8abc91e52bc3ab3ee8a4
fengmingming <1053931671@qq.com>
2024-11-25 23:33:46 +0800

    定义消息

--- src/main/java/boluo/im/Bootstrap.java
+++ src/main/java/boluo/im/Bootstrap.java
@@ -3,10 +3,7 @@ package boluo.im;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.messaging.rsocket.RSocketRequester;
-import org.springframework.messaging.rsocket.RSocketStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.pattern.PathPatternRouteMatcher;
 
 @SpringBootApplication
 public class Bootstrap {
